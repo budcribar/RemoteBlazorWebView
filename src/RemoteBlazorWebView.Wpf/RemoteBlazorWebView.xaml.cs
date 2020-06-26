bf2fc10c0de5cd7d4785203309d0ebe99537424f
Bud Cribar <budcribar@msn.com>
2020-06-26 07:11:45 -0600

    cleanup

--- src/RemoteBlazorWebView.Wpf/RemoteBlazorWebView.xaml.cs
+++ src/RemoteBlazorWebView.Wpf/RemoteBlazorWebView.xaml.cs
@@ -2,21 +2,12 @@
 using Microsoft.JSInterop;
 using PeakSwc.RemoteableWebWindows;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace RemoteBlazorWebView.Wpf
 {
