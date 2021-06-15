b8d47694b945fa1b4436366e51138636cd997ae6
Bud Cribar <budcribar@msn.com>
2021-06-15 08:56:48 -0600

    Cleanup

--- src/RemoteBlazorWebView.Wpf/RemoteBlazorWebView.cs
+++ src/RemoteBlazorWebView.Wpf/RemoteBlazorWebView.cs
@@ -49,11 +49,6 @@ namespace RemoteBlazorWebView.Wpf
         public static readonly DependencyProperty IsRestartingProperty =
             DependencyProperty.Register(nameof(IsRestarting), typeof(bool), typeof(RemoteBlazorWebView), new PropertyMetadata(false));
 
-        /// <summary>
-        /// Path to the host page within the application's static files. For example, <code>wwwroot\index.html</code>.
-        /// This property must be set to a valid value for the Blazor components to start.
-        /// </summary>
-        /// 
         private static void OnServerUriPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((RemoteBlazorWebView)d).OnServerUriPropertyChanged(e);
 
         private void OnServerUriPropertyChanged(DependencyPropertyChangedEventArgs _) { }
@@ -106,88 +101,6 @@ namespace RemoteBlazorWebView.Wpf
                 //else
                 //    MainBlazorWebView.Loaded -= value;
             }
-        }
-
-        private readonly ViewModel model = new();
-        static RemoteBlazorWebView() { }
-
-        public RemoteBlazorWebView()
-        {
-            //SetValue(RootComponentsProperty, new ObservableCollection<RootComponent>());
-            //RootComponents.CollectionChanged += HandleRootComponentsCollectionChanged;
-            // TODO
-
-            model.ShowHyperlink = "Hidden";
-         
-            DataContext = model;
-        }
-
-
-        private void HandleRootComponentsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs eventArgs)
-        {
-            Services = this.Services;
-            RootComponents.ToList().ForEach(x => RootComponents.Add(x));
-            HostPage = HostPage;
-           
-            if (ServerUri == null)
-            {
-                //innerBlazorWebView = MainBlazorWebView;
-                model.ShowHyperlink = "Hidden";
-            }
-            else 
-            {
-                ServerUri = ServerUri;
-                if (Id == default) Id = Guid.NewGuid();
-                Id = Id;
-                model.Uri = $"{ServerUri}app/{Id}";
-                model.ShowHyperlink = IsRestarting ? "Hidden" : "Visible";
-            }
-        }
-
-        //public event EventHandler<string> OnWebMessageReceived
-        //{
-        //    add
-        //    {
-              
-        //        //if (this.innerBlazorWebView != null)
-        //        //    this.innerBlazorWebView.OnWebMessageReceived += value;
-        //    }
-
-        //    remove
-        //    {
-        //        //if (this.innerBlazorWebView != null)
-        //        //    this.innerBlazorWebView.OnWebMessageReceived -= value;
-        //    }
-        //}
-
-        //public void Invoke(Action callback)
-        //{
-        //    // TODO
-        //    //innerBlazorWebView?.Invoke(callback);
-        //}
-
-        public void NavigateToUrl(string _)
-        {
-            // TODO
-            //innerBlazorWebView?.NavigateToUrl(url);
-        }
-
-        //public void SendMessage(string message)
-        //{
-        //   // TODO
-        //   //innerBlazorWebView?.SendMessage(message);
-        //}
-
-        //public void ShowMessage(string title, string message)
-        //{
-        //    // TODO
-        //    //innerBlazorWebView?.ShowMessage(title, message);
-        //}      
-
-        private void Hyperlink_Click(object sender, RoutedEventArgs e)
-        {
-            model.ShowHyperlink = "Hidden";
-            RemotableWebWindow.StartBrowser(this);  
         }
 
         public void Restart()
