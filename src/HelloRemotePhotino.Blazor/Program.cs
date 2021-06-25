ba86231ae818bc4e1ee8656cbcf59a937df60e79
Bud Cribar <budcribar@msn.com>
2021-06-25 16:52:00 -0600

    Don't specify guid

--- src/HelloRemotePhotino.Blazor/Program.cs
+++ src/HelloRemotePhotino.Blazor/Program.cs
@@ -11,8 +11,9 @@ namespace HelloRemotePhotino.Blazor
         {
             //var pw = new PhotinoWindow("Hello Remote Photino Blazor!");
             //var pw = new RemotePhotinoWindow(null, "wwwroot/index.html", "Hello Remote Photino Blazor!", default, (o) => { o.WebMessageReceivedHandler = (s, e) => { Console.WriteLine($"Web Message {e}"); }; });
+            //var pw = new RemoteBlazorWebWindow(new Uri("https://localhost:443"), "wwwroot/index.html", "Hello Remote Photino Blazor!", new Guid("BC7D925A-2F19-495A-A41E-DDC3C0187071"), (o) => { o.WebMessageReceivedHandler = (s, e) => { Console.WriteLine($"Web Message {e}"); }; });
 
-            var pw = new RemoteBlazorWebWindow(new Uri("https://localhost:443"), "wwwroot/index.html", "Hello Remote Photino Blazor!", new Guid("BC7D925A-2F19-495A-A41E-DDC3C0187071"), (o) => { o.WebMessageReceivedHandler = (s, e) => { Console.WriteLine($"Web Message {e}"); }; });
+            var pw = new RemoteBlazorWebWindow(new Uri("https://localhost:443"), "wwwroot/index.html", "Hello Remote Photino Blazor!", default);
             ComponentsDesktop.Run<Startup>(pw);
 
             //var pw = new RemotePhotinoWindow(new Uri("https://webserverstransporter.com:443"), "wwwroot/index.html", "Hello Remote Photino Blazor!", default, (o)=> { o.WebMessageReceivedHandler = (s, e) => { Console.WriteLine($"Web Message {e}"); }; });
