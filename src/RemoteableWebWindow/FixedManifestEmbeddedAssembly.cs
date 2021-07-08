1f42041c5c848d5f3124d95689e8f0619876afa7
Bud Cribar <budcribar@msn.com>
2021-07-08 14:37:45 -0600

    Embedded files working

---
+++ src/RemoteableWebWindow/FixedManifestEmbeddedAssembly.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace PeakSWC.RemoteableWebView
+{
+    public class FixedManifestEmbeddedAssembly : Assembly
+    {
+        // See:
+        // - https://github.com/dotnet/aspnetcore/issues/29306
+        // - https://github.com/dotnet/aspnetcore/blob/master/src/FileProviders/Embedded/src/build/netstandard2.0/Microsoft.Extensions.FileProviders.Embedded.targets
+        // - https://github.com/dotnet/aspnetcore/tree/master/src/FileProviders/Manifest.MSBuildTask/src
+        // - https://github.com/dotnet/aspnetcore/blob/master/src/FileProviders/Embedded/src/Manifest/ManifestParser.cs
+
+        private const string ManifestName = "Microsoft.Extensions.FileProviders.Embedded.Manifest.xml";
+
+        private readonly Assembly _inner;
+
+        public FixedManifestEmbeddedAssembly(Assembly inner) => _inner = inner;
+
+        [SuppressMessage("Publish", "IL3000", Justification = "Handled by caller.")]
+        public override string Location => _inner.Location;
+
+        public override AssemblyName GetName() => _inner.GetName();
+
+        public override Stream GetManifestResourceStream(string name)
+        {
+            var stream = _inner.GetManifestResourceStream(name);
+
+            if (name != ManifestName || stream == null)
+                return stream;
+
+            using var reader = new StreamReader(stream);
+
+            var xml = XDocument.Parse(reader.ReadToEnd());
+
+            var invalidNode = xml
+                .Descendants("File")
+                .FirstOrDefault(node => string.IsNullOrEmpty(node.Attribute("Name")?.Value));
+
+            if (invalidNode == null)
+            {
+                stream.Position = 0;
+                return stream;
+            }
+
+            invalidNode.Remove();
+
+            return new MemoryStream(Encoding.UTF8.GetBytes(xml.ToString()));
+        }
+    }
+}
