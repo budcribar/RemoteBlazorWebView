c3501c4e54cc833ef9863f75176c09c542a861b6
Bud Cribar <budcribar@msn.com>
2021-07-10 10:47:47 -0600

    Revert "Add path the cert file"
    This reverts commit 96d9bb05b61d354640465af9eb9e9aa58bde2117.

--- src/RemoteableWebWindowService/Program.cs
+++ src/RemoteableWebWindowService/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System.IO;
 using System.Net;
-using System.Reflection;
 
 namespace PeakSWC.RemoteableWebView
 {
@@ -24,10 +23,8 @@ namespace PeakSWC.RemoteableWebView
                         //  Unable to start Kestrel Socket Exception(10013) - need to stop IIS
                         // MUST bind to internal IP address !!
 
-                        var certFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly()?.Location) ?? string.Empty, "cert.pfx");
-
-                        if (File.Exists(certFile))
-                            options.Listen(IPAddress.Parse("10.1.0.4"), 443, lo => { lo.UseHttps(certFile, string.Empty); });
+                        if (File.Exists("cert.pfx"))
+                            options.Listen(IPAddress.Parse("10.1.0.4"), 443, lo => { lo.UseHttps("cert.pfx", string.Empty); });
                         else
                             options.Listen(IPAddress.Loopback, 443, listenOptions => { listenOptions.UseHttps(); });
                     });
