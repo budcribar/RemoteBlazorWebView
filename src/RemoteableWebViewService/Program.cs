e7c630da0de9e8348ef8ab21ada4c48b489f623a
Bud Cribar <budcribar@msn.com>
2021-08-17 14:10:20 -0600

    Use appsettings for Kestrel configuration

--- src/RemoteableWebViewService/Program.cs
+++ src/RemoteableWebViewService/Program.cs
@@ -16,18 +16,6 @@ namespace PeakSWC.RemoteableWebView
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.ConfigureKestrel(options =>
-                    {
-
-                        // Webwindow.westus.cloudapp.azure.com Private IP address
-                        //  Unable to start Kestrel Socket Exception(10013) - need to stop IIS
-                        // MUST bind to internal IP address !!
-
-                        if (File.Exists("cert.pfx"))
-                            options.Listen(IPAddress.Parse("10.1.0.4"), 443, lo => { lo.UseHttps("cert.pfx", string.Empty); });
-                        else
-                            options.Listen(IPAddress.Loopback, 443, listenOptions => { listenOptions.UseHttps(); });
-                    });
 
                     // Comment out for App Service
                     webBuilder.UseKestrel();
--- src/RemoteableWebViewService/ServiceState.cs
+++ src/RemoteableWebViewService/ServiceState.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.IO;
 using System.Threading;
 using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace PeakSWC.RemoteableWebView
 {
