59199a992cbd297b3eb88cc9e1f38e71b6bbcc94
Bud Cribar <budcribar@msn.com>
2022-01-03 08:20:20 -0700

    Cleanup

--- EditWebView/Program.cs
+++ EditWebView/Program.cs
@@ -5,29 +5,24 @@ string maui = "maui-6.0.101-preview.11.3";
 string inputDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", maui, @"src\BlazorWebView\src\WindowsForms");
 string outputDir = "../../../../src/RemoteBlazorWebView.WinForms";
 
-if (!Directory.Exists(outputDir))
-    throw new Exception("Can't locate output directory");
-
-foreach(var f in Directory.EnumerateFiles(inputDir))
-{
-    if (f.EndsWith(".csproj")) continue;
-
-    Editor editor = new Editor(f);
-    editor.Edit();  
-    editor.WriteAllText(outputDir);
-}
+ProcessFiles(inputDir, outputDir);
 
 inputDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", maui, @"src\BlazorWebView\src\Wpf");
 outputDir = "../../../../src/RemoteBlazorWebView.Wpf";
 
-if (!Directory.Exists(outputDir))
-    throw new Exception("Can't locate output directory");
+ProcessFiles(inputDir, outputDir);
 
-foreach (var f in Directory.EnumerateFiles(inputDir))
+static void ProcessFiles(string inputDir, string outputDir)
 {
-    if (f.EndsWith(".csproj")) continue;
+    if (!Directory.Exists(outputDir))
+        throw new Exception("Can't locate output directory");
 
-    Editor editor = new Editor(f);
-    editor.Edit();
-    editor.WriteAllText(outputDir);
+    foreach (var f in Directory.EnumerateFiles(inputDir))
+    {
+        if (f.EndsWith(".csproj")) continue;
+
+        Editor editor = new Editor(f);
+        editor.Edit();
+        editor.WriteAllText(outputDir);
+    }
 }
