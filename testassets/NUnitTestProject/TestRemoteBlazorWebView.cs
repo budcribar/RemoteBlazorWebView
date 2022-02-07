a8e2fb17f07e27b5d27116099f3a4d0b3ea7e858
Bud Cribar <budcribar@msn.com>
2022-02-07 09:00:50 -0700

    Add TestRemoteBlazorWebView Test2Client5Refresh

--- testassets/NUnitTestProject/TestRemoteBlazorWebView.cs
+++ testassets/NUnitTestProject/TestRemoteBlazorWebView.cs
@@ -7,10 +7,6 @@ namespace WebdriverTestProject
    [TestClass]
     public class TestRemoteBlazorWebView : TestRemoteBlazorWpf
     {
-        public override void Test2Client5Refresh()
-        {
-           // TODO Remove this to add back
-        }
         public override Process CreateClient()
         {
             return Utilities.StartRemoteBlazorWebViewApp();
