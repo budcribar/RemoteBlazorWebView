394b58511ae9fc64e128f5bcbea35855e8977a61
Raymond Cribar <raymond.cribar@hp.com>
2022-11-08 06:14:08 -0700

    Update dependencies

--- src/SharedSource/BlazorWebViewServiceCollectionExtensions.cs
+++ src/SharedSource/BlazorWebViewServiceCollectionExtensions.cs
@@ -54,14 +54,14 @@ namespace PeakSWC.RemoteBlazorWebView
 #endif
 		}
 
-/// <summary>
-/// Enables Developer tools on the underlying WebView controls.
-/// </summary>
-/// <param name="services">The <see cref="IServiceCollection"/>.</param>
-/// <returns>The <see cref="IServiceCollection"/>.</returns>
-public static IServiceCollection AddRemoteBlazorWebViewDeveloperTools(this IServiceCollection services)
-{
-	return services.AddSingleton<BlazorWebViewDeveloperTools>(new BlazorWebViewDeveloperTools { Enabled = true });
-}
+		/// <summary>
+		/// Enables Developer tools on the underlying WebView controls.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+		/// <returns>The <see cref="IServiceCollection"/>.</returns>
+		public static IServiceCollection AddRemoteBlazorWebViewDeveloperTools(this IServiceCollection services)
+		{
+			return services.AddSingleton<BlazorWebViewDeveloperTools>(new BlazorWebViewDeveloperTools { Enabled = true });
+		}
 	}
 }
