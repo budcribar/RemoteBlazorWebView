082a9c8a1f2b4cc6fea43dc8b79e68d2606bd360
Raymond Cribar <raymond.cribar@hp.com>
2023-05-11 16:07:22 -0600

    No changes in maui07.0.86

--- src/EditWebView/Program.cs
+++ src/EditWebView/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EditWebView;
 
-string maui = "maui-7.0.81";
+string maui = "maui-7.0.86";
 string inputDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", maui, @"src\BlazorWebView\src\WindowsForms");
 string outputDir = "../../../../RemoteBlazorWebView.WinForms";
 
