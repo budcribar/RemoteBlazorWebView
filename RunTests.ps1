90ad65995ee2a108c22fd1235da0c579af2c2e53
Raymond Cribar <raymond.cribar@hp.com>
2023-05-24 14:57:39 -0600

    Fix cleanup

--- RunTests.ps1
+++ RunTests.ps1
@@ -39,7 +39,7 @@ Write-Host -ForegroundColor GREEN "Clean artifacts"
 
 Get-ChildItem .\src\RemoteWebView.Blazor.JS -include node_modules,dist -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }
 Get-ChildItem .\src -Exclude RemoteWebView.Blazor.JS | Get-ChildItem -include bin,obj,publish,publishNoAuth,publishAuth,artifacts   -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }
-Get-ChildItem ..\RemoteBlazorWebViewTutorial\ -include bin,obj,publish,publishEmbedded, embedded -Exclude EBWebView -Recurse -Force | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }
+Get-ChildItem ..\RemoteBlazorWebViewTutorial -include bin,obj,publish,publishEmbedded, embedded -Exclude EBWebView -Recurse -Force | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }
 Get-ChildItem ${env:HOMEPATH}\.nuget\packages\Peak* | remove-item -Force -Recurse
 
 Write-Host -ForegroundColor GREEN "Install node_modules"
