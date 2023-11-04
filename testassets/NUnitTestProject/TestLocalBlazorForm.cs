de7a198acee5a9e57d77def914db235329fb6402
Bud Cribar <budcribar@msn.com>
2023-11-04 08:36:46 -0600

    WIP

--- testassets/NUnitTestProject/TestLocalBlazorForm.cs
+++ testassets/NUnitTestProject/TestLocalBlazorForm.cs
@@ -24,12 +24,13 @@ namespace WebdriverTestProject
         {
             //Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--remote-debugging-port=9222");
             var webview2 = Path.Combine(Path.GetDirectoryName(Environment.ProcessPath ?? "") ?? "", "WebView2");
-            Environment.SetEnvironmentVariable("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", webview2);
-            Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", webview2);
+            //Environment.SetEnvironmentVariable("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", webview2);
+            //Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", webview2);
 
             startingDirectory = Directory.GetCurrentDirectory();
-            Directory.SetCurrentDirectory(Path.GetDirectoryName(BinaryLocation()) ?? string.Empty);
-            driver = new EdgeDriver(new EdgeOptions { UseWebView = true, BinaryLocation = Path.GetFileName(this.BinaryLocation()) });
+            var binary = BinaryLocation();
+            //Directory.SetCurrentDirectory(Path.GetDirectoryName(BinaryLocation()) ?? string.Empty);
+            driver = new EdgeDriver(new EdgeOptions { UseWebView = true, BinaryLocation = BinaryLocation()  });
             // Wait for page to load 
             Thread.Sleep(1000);
         }
