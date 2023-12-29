f63546366d9e6c2f71f974bb186858392b98aba6
Bud Cribar <budcribar@msn.com>
2023-12-29 12:37:30 -0700

    Don't include RemoteBlazorWebViewTutorial.exe in zip file

--- RunTests.ps1
+++ RunTests.ps1
@@ -138,7 +138,7 @@ if ($Build -ne $true)
 
 # zip up files for github
 $compress = @{
-  Path = "src\RemoteWebViewService\bin\publishNoAuth\RemoteWebViewService.exe", "..\RemoteBlazorWebViewTutorial\RemoteBlazorWebViewTutorial.WinFormsApp\bin\publishEmbedded\RemoteBlazorWebViewTutorial.WinFormsApp.exe","..\RemoteBlazorWebViewTutorial\RemoteBlazorWebViewTutorial.WpfApp\bin\publishEmbedded\RemoteBlazorWebViewTutorial.WpfApp.exe","..\RemoteBlazorWebViewTutorial\RemoteBlazorWebViewTutorial\bin\publishEmbedded\RemoteBlazorWebViewTutorial.exe", "README.txt"
+  Path = "src\RemoteWebViewService\bin\publishNoAuth\RemoteWebViewService.exe", "..\RemoteBlazorWebViewTutorial\RemoteBlazorWebViewTutorial.WinFormsApp\bin\publishEmbedded\RemoteBlazorWebViewTutorial.WinFormsApp.exe","..\RemoteBlazorWebViewTutorial\RemoteBlazorWebViewTutorial.WpfApp\bin\publishEmbedded\RemoteBlazorWebViewTutorial.WpfApp.exe", "README.txt"
   CompressionLevel = "Fastest"
   DestinationPath = "artifacts\Release.Zip"
 }
