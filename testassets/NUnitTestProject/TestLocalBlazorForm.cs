ddb3c193078aee79d207eb99142dd89f7eb51f64
Bud Cribar <budcribar@msn.com>
2023-12-29 12:35:24 -0700

    Fix Binary location

--- testassets/NUnitTestProject/TestLocalBlazorForm.cs
+++ testassets/NUnitTestProject/TestLocalBlazorForm.cs
@@ -22,18 +22,19 @@ namespace WebdriverTestProject
         [TestInitialize]
         public void Setup()
         {
-            //Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--remote-debugging-port=9222");
+            Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--remote-debugging-port=9222");
             var webview2 = Path.Combine(Path.GetDirectoryName(Environment.ProcessPath ?? "") ?? "", "WebView2");
-            //Environment.SetEnvironmentVariable("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", webview2);
+            Environment.SetEnvironmentVariable("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", webview2);
             //Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", webview2);
 
             startingDirectory = Directory.GetCurrentDirectory();
             var binary = BinaryLocation();
-            //Directory.SetCurrentDirectory(Path.GetDirectoryName(BinaryLocation()) ?? string.Empty);
+            var executable = Path.GetFileName(binary);
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(BinaryLocation()) ?? string.Empty);
             var options = new EdgeOptions
             {
                 UseWebView = true,
-                BinaryLocation = BinaryLocation()
+                BinaryLocation = executable
             };
             options.SetLoggingPreference("performance", LogLevel.All);
          
