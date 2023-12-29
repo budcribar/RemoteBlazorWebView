458171d87832c30fb04cbedacc473eb9e59f63d2
Bud Cribar <budcribar@msn.com>
2023-12-29 12:32:38 -0700

    Update packages

--- src/EditWebView/Program.cs
+++ src/EditWebView/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EditWebView;
 
-string maui = "maui-7.0.100";
+string maui = "maui-7.0.101";
 string inputDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", maui, @"src\BlazorWebView\src\WindowsForms");
 string outputDir = "../../../../RemoteBlazorWebView.WinForms";
 
