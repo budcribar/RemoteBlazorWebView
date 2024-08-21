d78dee5974856892bfddbf5fd034bda18c6287dd
Bud Cribar <budcribar@msn.com>
2024-08-20 19:19:13 -0600

    Add some benchmarks

--- src/Benchmarks/Startup/Program.cs
+++ src/Benchmarks/Startup/Program.cs
@@ -3,11 +3,85 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Google.Protobuf.WellKnownTypes;
+using PeakSWC.RemoteWebView;
+using Grpc.Core;
 
 namespace ServerStartupTimer
 {
     class Program
     {
+        public static long GetFileSize(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not found.", filePath);
+            }
+
+            FileInfo fileInfo = new FileInfo(filePath);
+            return fileInfo.Length;
+        }
+
+        public static async void TestCreateWebView()
+        {
+            string grpcUrl = @"https://localhost:5001/";
+            WebViewIPC.WebViewIPCClient? client;
+            var channel = GrpcChannel.ForAddress(grpcUrl);
+            client = new WebViewIPC.WebViewIPCClient(channel);
+
+            int loops = 125;
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < loops; i++)
+            {
+                var response = client.CreateWebView(new CreateWebViewRequest { Id = i.ToString() });
+
+                await foreach (var message in response.ResponseStream.ReadAllAsync())
+                {
+                    if (message.Response == "created:")
+                        break;
+                    Console.WriteLine(message.Response);
+                }
+
+                //response.Dispose();
+                //var reader = client.FileReader();
+                //reader.RequestStream.WriteAsync(new FileReadRequest { })
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Avg Time for {loops} CreateWebViewRequest request: {stopwatch.ElapsedMilliseconds / loops} ms per request");
+
+            var ids = await client.GetIdsAsync(new Empty());
+            Debug.Assert(ids.Responses.Count == loops);
+
+            stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < loops; i++)
+            {
+                client.Shutdown(new IdMessageRequest { Id = i.ToString() });
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Avg Time for {loops} Shutdown request: {stopwatch.ElapsedMilliseconds / loops} ms per request");
+            ids = await client.GetIdsAsync(new Empty());
+            Debug.Assert(ids.Responses.Count == 0);
+        }
+        public static async void TestClientIPCService()
+        {
+            int loops = 125; // hangs after 127
+            string grpcUrl = @"https://localhost:5001/";
+            ClientIPC.ClientIPCClient? client;
+            var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+            var channel = GrpcChannel.ForAddress(grpcUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
+            client = new ClientIPC.ClientIPCClient(channel);
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < loops; i++)
+            {
+                var response = await client!.GetServerStatusAsync(new Empty { });
+                //Console.WriteLine(i);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Avg Time for {loops} GetServerStatusAsync request: {stopwatch.ElapsedMilliseconds/loops} ms per request");
+        }
+
         static async Task Main(string[] _)
         {
 
@@ -32,9 +106,11 @@ namespace ServerStartupTimer
 
             var processStartInfo = new ProcessStartInfo
             {
-                FileName = @"..\..\..\..\..\RemoteWebViewService\bin\publishNoAuth\RemoteWebViewService.exe",
+                FileName = @"..\..\..\..\..\..\RemoteWebViewService\bin\publishNoAuth\RemoteWebViewService.exe",
                 RedirectStandardOutput = true
             };
+
+            Console.WriteLine($"Server File Size {GetFileSize(processStartInfo.FileName)}");
 
             using (var process = Process.Start(processStartInfo))
             {
@@ -45,7 +121,7 @@ namespace ServerStartupTimer
                 // Poll the port
                 //await PollPort(serverHost, serverPort);
 
-                string url = $"https://{serverHost}:{serverPort}/test";
+                string url = $"https://{serverHost}:{serverPort}";
                 using (var httpClient = new HttpClient())
                 {
                     // Poll for a successful HTTP request
@@ -70,8 +146,20 @@ namespace ServerStartupTimer
                     await PollHttpRequest(httpClient, url);
                     stopwatch.Stop();
                     Console.WriteLine($"Time to fourth request: {stopwatch.ElapsedMilliseconds} ms");
-                }
 
+                    stopwatch.Restart();
+                    for (int i = 0; i < 500; i++)
+                    {
+                        var res = await httpClient.GetAsync(url);
+                        //Console.WriteLine(i);
+                    }
+                    stopwatch.Stop();
+                    Console.WriteLine($"Time for 500 requests: {stopwatch.ElapsedMilliseconds} ms");
+                    TestCreateWebView();
+                   // TestClientIPCService();
+
+                }
+                Console.ReadKey();
                 process?.Kill(); // Stop the server
             }
         }
--- src/Benchmarks/Startup/results.md
+++ src/Benchmarks/Startup/results.md
@@ -23,3 +23,17 @@
 | 2nd  | 6        | 5        | 1        |
 | 3rd  | 6        | 5        | 1        |
 | 4th  | 1        | 1        | 0        |
+
+
+### dotnet9 Release Mode Size 32,243,712
+
+| Run  | Result 1 | Result 2 | Result 3 |
+|------|----------|----------|----------|
+| 1st  | 2241ms   |  2260ms  | 2272     |
+| 2nd  | 1ms      |     1ms  | 1        |
+| 3rd  | 1ms      |     1ms  | 1        |
+| 4th  | 0ms      |     1ms  | 0        |
+| 500  | 96 ms    |    91ms  | 80ms     |
+|      | 24ms     |    24ms  | 24ms     | 100 GetServerStatusAsync
+|      | 20ms     |    20ms  | 20ms     | 125 GetServerStatusAsync
+|      | 34ms     |    34ms  | 34ms     | 125 CreateWebViewRequest
