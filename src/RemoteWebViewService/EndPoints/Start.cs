d6f5aae8993c7c5719607fd3b30acb1a020089d7
Bud Cribar <budcribar@msn.com>
2024-09-21 05:32:06 -0600

    Move endpoints

---
+++ src/RemoteWebViewService/EndPoints/Start.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using PeakSWC.RemoteWebView.Pages;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Threading.Channels;
+
+namespace PeakSWC.RemoteWebView.EndPoints
+{
+    public static partial class Endpoints
+    {
+        public static RequestDelegate Start()
+        {
+            return async context =>
+            {
+                string guid = context.Request.RouteValues["id"]?.ToString() ?? string.Empty;
+                var serviceDictionary = context.RequestServices.GetRequiredService<ConcurrentDictionary<string, ServiceState>>();
+                var serviceStateChannel = context.RequestServices.GetRequiredService<ConcurrentDictionary<string, Channel<string>>>();
+
+                if (serviceDictionary.TryGetValue(guid, out var serviceState))
+                {
+                    if (serviceState.InUse)
+                    {
+                        context.Response.StatusCode = 400;
+                        context.Response.ContentType = "text/html";
+                        await context.Response.WriteAsync(LockedPage.Html(serviceState.User, guid)).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        serviceState.Cookies = context.Request.Cookies;
+                        serviceState.InUse = true;
+                        serviceState.User = context.User.GetDisplayName() ?? "";
+
+                        if (serviceState.IPC.ClientResponseStream != null)
+                            await serviceState.IPC.ClientResponseStream.WriteAsync(new WebMessageResponse { Response = "browserAttached:" }).ConfigureAwait(false);
+                        // Update Status
+                        foreach (var channel in serviceStateChannel.Values)
+                            await channel.Writer.WriteAsync($"Connect:{guid}").ConfigureAwait(false);
+
+                        context.Response.Redirect($"/{guid}");
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync(RestartFailedPage.Html(guid, false)).ConfigureAwait(false);
+                }
+            };
+        }
+
+    }
+}
---
+++ src/RemoteWebViewService/EndPoints/Wait.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using PeakSWC.RemoteWebView.Pages;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PeakSWC.RemoteWebView.EndPoints
+{
+    public static partial class Endpoints
+    {
+        public static RequestDelegate Wait()
+        {
+           
+            return async context =>
+            {
+                string guid = context.Request.RouteValues["id"]?.ToString() ?? string.Empty;
+                var serviceDictionary = context.RequestServices.GetRequiredService<ConcurrentDictionary<string, ServiceState>>();
+
+                for (int i = 0; i < 30; i++)
+                {
+                    if (serviceDictionary.ContainsKey(guid))
+                    {
+                        await context.Response.WriteAsync($"Wait completed").ConfigureAwait(false);
+                        return;
+                    }
+
+                    await Task.Delay(1000).ConfigureAwait(false);
+                }
+
+                context.Response.StatusCode = 400;
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(RestartFailedPage.Fragment(guid)).ConfigureAwait(false);
+            };
+        }
+
+    }
+}
