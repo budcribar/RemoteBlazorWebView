e4237953dd4ac8d6829c8425c986b5efad0237af
Bud Cribar <budcribar@msn.com>
2025-05-09 17:30:50 -0600

    allow running of profiled server

--- test/FileSyncServer.Tests/Utilities.cs
+++ test/FileSyncServer.Tests/Utilities.cs
@@ -62,8 +62,9 @@ namespace WebdriverTestProject
         {
             Stopwatch sw = new();
             sw.Start();
-
+#if !PROFILED_SERVER
             Process.GetProcesses().FirstOrDefault(p => p.ProcessName == "RemoteWebViewService")?.Kill();
+#endif
 #if DEBUG_SERVER
             var relative = @"RemoteBlazorWebView\src\RemoteWebViewService\bin\x64\Debug\net10.0";
 #else
@@ -76,8 +77,9 @@ namespace WebdriverTestProject
             process.StartInfo.FileName = Path.GetFullPath(f);
             process.StartInfo.WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), relative);
             process.StartInfo.UseShellExecute = true;
-
+#if !PROFILED_SERVER
             process.Start();
+#endif
             Console.WriteLine($"Started server in {sw.Elapsed}");
             return process;
         }
